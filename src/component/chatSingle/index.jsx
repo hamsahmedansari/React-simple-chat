0857d1ae1fd3b1840b152cd1d6383318cd334dbc
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-26 23:45:57 +0500

    UPDATE: scrollBottom on adding message

--- src/component/chatSingle/index.jsx
+++ src/component/chatSingle/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "./styls.scss";
+import "./style.scss";
 
 class ChatSingle extends Component {
   constructor(props) {
